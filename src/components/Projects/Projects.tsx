9b006e7730f421e4fabd922ecbfbfc3702d9b4f5
aslanchan7 <aslanchan7@gmail.com>
2025-08-31 19:43:22 -0700

    projects section complete

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,54 @@
+import styles from "./Projects.module.css"
+import projects from "../../data/projects.json"
+import { IoPeople, IoTime, IoChevronForward } from "react-icons/io5"
+
+export const Projects = () => {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className={styles.title}>
+        Projects
+      </h1>
+
+      <div className={styles.projectsContainer}>
+        {projects.map((project, id) => {
+          return (
+            <div key={id} className={styles.projectCard}>
+              <img src={`/assets/Projects/${project.imageSrc}`} />
+                <div className={styles.briefInfo}>
+                  <IoPeople className={styles.teamSizeIcon} />
+                  <p>{project.teamSize}</p>
+                  <IoTime className={styles.timeIcon} />
+                  <p>{project.time}</p>
+                </div>
+
+              <div className={styles.projectTitleContainer}>
+                <h2 className={styles.projectTitle}>
+                  {project.title}
+                </h2>
+
+                <IoChevronForward className={styles.chevronForwardIcon} size={24}/>
+              </div>
+
+              <p className={styles.projectDescription}>
+                {project.description}
+              </p>
+
+              <div className={styles.skillsContainer}>
+                {project.skills.map((skill, id) => {
+                  return <div key={id} className={styles.skillCard}>
+                      <p>{skill}</p>
+                    </div>
+                })}
+              </div>
+            </div>
+          )
+        })}
+
+      </div>
+
+      <a href="/projects" className={styles.allProjectsBtn}>
+        Browse All Projects
+      </a>
+    </section>
+  )
+}
